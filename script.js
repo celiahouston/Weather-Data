e964bb14f68952298546d4c27b6c28b5f99610d4
celiahouston <celia.houston99@gmail.com>
2023-12-15 18:42:38 +0000

    removed commented out unused code and created history search button

--- script.js
+++ script.js
@@ -10,97 +10,75 @@ document.addEventListener('DOMContentLoaded', function () {
             getCityWeather(cityName);
         } else {
             console.log('Failed to enter city name');
-            alert('Please enter city name.')
-       }
+            alert('Please enter city name.');
+        }
     });
-}); 
 
-function addToSearchHistory(city) {
-    var historyElement = document.getElementById('history');
-    var historyItem = document.createElement('button');
-    historyItem.textContent = city;
-    historyItem.classList.add('btn', 'history-item');
-    historyItem.addEventListener('click', function () {
-        getCityWeather(city);
-    });
-    historyElement.appendChild(historyItem);
-}
+    function addToSearchHistory(city) {
+        var history = localStorage.getItem('searchHistory');
+        var historyArray = history ? JSON.parse(history) : [];
 
-function getCityWeather(city) {
-    var cityWeatherEndpoint = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=0239f425bc2ae16f6f0c6a0c55908966`;
+        if (!historyArray.includes(city)) {
+            historyArray.push(city);
+            localStorage.setItem('searchHistory', JSON.stringify(historyArray));
+        }
 
-    fetch(cityWeatherEndpoint)
-        .then((response) => {
-            if (!response.ok) {
-                throw new Error('City not found.');
-            }
-            return response.json();
-        })
-        .then((data) => {
-            displayWeatherData(data);
-            addToSearchHistory(city);
-        })
-        .catch((error) => {
-            console.error('Error: ', error);
-            alert('City not found, please try again.');
+        displaySearchHistory();
+    }
+
+    function displaySearchHistory() {
+        var history = localStorage.getItem('searchHistory');
+        var historyArray = history ? JSON.parse(history) : [];
+        var historyElement = document.getElementById('history');
+
+        historyElement.innerHTML = '';
+
+        historyArray.forEach(function (city) {
+            var historyItem = document.createElement('button');
+            historyItem.textContent = city;
+            historyItem.className = 'history-item btn btn-primary m-1'; // Setting classes using className
+            historyItem.addEventListener('click', function () {
+                getCityWeather(city);
+            });
+            historyElement.appendChild(historyItem);
         });
-}
+    }
 
-function displayWeatherData(data) {
-    var weatherInfo = document.getElementById('today');
-    var cityName = data.name; 
-    var date = new Date(data.dt * 1000); 
-    var iconCode = data.weather[0].icon; 
-    var iconUrl = `http://openweathermap.org/img/w/${iconCode}.png`;
+    function getCityWeather(city) {
+        var cityWeatherEndpoint = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=0239f425bc2ae16f6f0c6a0c55908966`;
 
-    weatherInfo.innerHTML = `
+        fetch(cityWeatherEndpoint)
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error('City not found.');
+                }
+                return response.json();
+            })
+            .then((data) => {
+                displayWeatherData(data);
+                addToSearchHistory(city);
+            })
+            .catch((error) => {
+                console.error('Error: ', error);
+                alert('City not found, please try again.');
+            });
+    }
+
+    function displayWeatherData(data) {
+        var weatherInfo = document.getElementById('weather-info');
+        var cityName = data.name;
+        var date = new Date(data.dt * 1000);
+        var iconCode = data.weather[0].icon;
+        var iconUrl = `http://openweathermap.org/img/w/${iconCode}.png`;
+
+        weatherInfo.innerHTML = `
     <h2>Weather Details for ${data.name}</h2>
     <p>Date: ${date.toDateString()}</p>
     <img src="${iconUrl}" alt="Weather Icon"> 
     <p>Temperature: ${data.main.temp}</p>
     <p>Humidity: ${data.main.humidity}</p>
     <p>Wind Speed: ${data.wind.speed} </p>
-    `; 
-}
-
-    fetch(cityWeatherEndpoint)
-        .then((response) => {
-            if (!response.ok) {
-                throw new Error('City not found.');
-            } return response.json();
-        })
-        .then((data) => {
-            console.log('Weather Data: ', JSON.stringify(data, null, 2));
-            displayWeatherData(data);
-            addToSearchHistory(city);
-        })
-        .catch((error) => {
-            console.error('Error: ', error);
-            alert('City not found, please try again.');
-        });
-
-function displayForecast(forecastData) {
-    var forecast = document.getElementById('forecast');
-    var forecastCards = 
-    forecast.innerHTML = '';
-
-    for (let index = 0; index < forecastData.length; index++) {
-        var forecast = forecastData.list[index];
-        // var forecastDate = 
-        var forecastItem = document.createElement('div');
-        forecastItem.classList.add('forecast-item');
-
-        // date append 
-
-        var Temperature = document.createElement('p');
-        Temperature.textContent = 'Temperature: ${forecast.main.temp}';
-        forecastItem.appendChild(Temperature);
-
-        var humidity = document.createElement('p');
-        humidity.textContent = 'Humidity: ${forecaste.main.humidity}';
-        forecastItem.appendChild(forecastItem);
+    `;
     }
-}
-
-
+});
 
